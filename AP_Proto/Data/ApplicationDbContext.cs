1b25ce44f53bdd78fbb9aa5630ac26a8256196f3
Jim <jshaun1989@hotmail.co.uk>
2021-04-11 12:17:24 +0100

    Added Contact View and Scaffolding

--- AP_Proto/Data/ApplicationDbContext.cs
+++ AP_Proto/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AP_Proto.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,6 +9,9 @@ namespace AP_Proto.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+
+        public DbSet<ContactModel> Contact { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
