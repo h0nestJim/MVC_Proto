fd27eeab26b1cb3cc7b9842cf7b7a61c132ffbbe
Jim <jshaun1989@hotmail.co.uk>
2021-04-11 11:18:51 +0100

    Initial Commit

---
+++ AP_Proto/Models/ContactModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AP_Proto.Models
+{
+    public class ContactModel
+    {   
+        //class structure for contacts. 
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Fname { get; set; }
+        [Required]
+        public string Sname { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Range(11,13)]
+        public string Tel { get; set; }
+    }
+}
